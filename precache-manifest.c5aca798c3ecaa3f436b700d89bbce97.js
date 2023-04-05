073110269a41b204c576044715e33368a1f899e3
Supakron Siwapanich <supakron.siw@digitalvalley.co.th>
2023-04-05 13:57:12 +0700

    Updates

---
+++ precache-manifest.c5aca798c3ecaa3f436b700d89bbce97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e9293f143fa628947f6108650f8564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc75bffb7630974d1852",
+    "url": "/static/css/main.bd02cd75.chunk.css"
+  },
+  {
+    "revision": "f328fe8a39c7d140e11d",
+    "url": "/static/js/2.eaf2b168.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "/static/js/2.eaf2b168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc75bffb7630974d1852",
+    "url": "/static/js/main.11b778b2.chunk.js"
+  },
+  {
+    "revision": "7c5e4ace7e9c5a53bd8e",
+    "url": "/static/js/runtime-main.06e93911.js"
+  }
+]);
